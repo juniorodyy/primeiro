d3c96d23024f94e83f9d6b4b3eea3eaa3d22fc56
juniorodyy <junior.ody2000@gmail.com>
2017-03-28 21:42:25 -0300

    segundos testes

--- calc.spec.js
+++ calc.spec.js
@@ -3,43 +3,23 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
 const { soma } = require('./calc');
-
-lab.test('retorno da soma de 2 e 3 deve ser 5', (done) => {
-
-    Code.expect(soma(2, 3)).to.equal(5);
+lab.test('não passando parâmetros', (done) => {
+    const r = soma();
+    expect(r.message).to.equal("parâmentos faltando")
+    done();
+});
+lab.test('somando números inteiros', (done) => {
+    expect(soma(2, 3)).to.equal(5);
+    expect(soma(20, 33)).to.equal(53);
     done();
 });
 
-lab.test('retorno da soma de 5 e 10 deve ser 15', (done) => {
-
-    Code.expect(soma(5,10)).to.equal(15);
+lab.test('somando números negativos', (done) => {
+    expect(soma(-2, -5)).to.equal(-7);
     done();
-  });
+});
+lab.test('retorno da soma de -5 e 10 é 5', (done) => {
 
-  lab.test('retorno da soma de nenhum numero é 5', (done) => {
-
-      Code.expect(soma(5,)).to.equal(5);
-      done();
-  });
-
-  lab.test('retorno da soma de -5 e 10 deve ser 10', (done) => {
-
-      Code.expect(soma(-5,10)).to.equal(10);
-      done();
-  });
-
-  lab.test('A soma de letras é igual a 0', (done) => {
-
-      Code.expect(soma(5,a)).to.equal(5);
-      done();
-  });
-  lab.test('retorno da soma de -5 e -10 deve ser -20', (done) => {
-
-      Code.expect(soma(-5,10)).to.equal(-20);
-      done();
-  });
-  lab.test('retorno da soma de deve ser ', (done) => {
-
-      Code.expect(soma(,)).to.equal();
-      done();
-  });
+    Code.expect(soma(-5,10)).to.equal(5);
+    done();
+});
